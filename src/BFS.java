158e5165dc71d6e79284e0c8e884010532d1c500
Roshan jha <141396860+roshan-jha-23@users.noreply.github.com>
2024-01-11 01:11:35 +0530

    Update BFS.java
    Solution added

--- src/BFS.java
+++ src/BFS.java
@@ -1,5 +1,29 @@
 public class BFS {
-    public static void main(String[] args) {
+    class Solution {
+    // Function to return Breadth First Traversal of given graph.
+    public ArrayList<Integer> bfsOfGraph(int v, ArrayList<ArrayList<Integer>> adj) {
+        // Code here
+        ArrayList<Integer> bfs=new ArrayList<>();
+        boolean[] vis=new boolean[v];
+        Queue<Integer> q=new LinkedList<>();
+         q.add(0);
+         vis[0] = true;
+        while(!q.isEmpty()){
+            Integer node=q.poll();
+            bfs.add(node);
+            for(Integer it:adj.get(node)){
+                if(!vis[it]){
+                    vis[it]=true;
+                    q.add(it);
+                }
+            }
+        }
+     
+        return bfs;
         
+    }
+}
+    public static void main(String[] args) {
+        //watever you want for your graph.
     }
 }
